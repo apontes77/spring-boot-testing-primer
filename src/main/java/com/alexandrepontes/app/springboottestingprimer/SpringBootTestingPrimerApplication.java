66af7b1b21dde3da94c04e9a25db123278960b4d
apontes77 <alexandrepontesgyn@gmail.com>
2021-10-01 05:39:36 -0300

    first commit

---
+++ src/main/java/com/alexandrepontes/app/springboottestingprimer/SpringBootTestingPrimerApplication.java
@@ -0,0 +1,13 @@
+package com.alexandrepontes.app.springboottestingprimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingPrimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestingPrimerApplication.class, args);
+	}
+
+}
